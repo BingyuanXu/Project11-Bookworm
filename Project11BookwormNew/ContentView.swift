5249fc09be08d741e8d59d6cffa3f1515ac9ef56
BingyuanXu <xubingyuan123@hotmail.com>
2020-06-13 22:43:41 -0500

    using core data to save and read object

--- Project11BookwormNew/ContentView.swift
+++ Project11BookwormNew/ContentView.swift
@@ -9,13 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+  @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+  @Environment(\.managedObjectContext) var moc
+  
+  var body: some View {
+    
+    VStack {
+      
+      List {
+        ForEach(students, id: \.self) { student in
+          Text(student.name ?? "Unknown")
+          
+        }
+      }
+      
+      Button("Add") {
+        let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+        let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+        
+        let chosenFirstName = firstNames.randomElement()!
+        let chosenLastName = lastNames.randomElement()!
+        
+        let student = Student(context: self.moc)
+        student.id = UUID()
+        student.name = "\(chosenFirstName) \(chosenLastName)"
+        try? self.moc.save()
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
